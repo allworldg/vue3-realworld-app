5a1f54306d4a7722b9a66835492ff94aa4b91c97
allworldg <944937881@qq.com>
2023-10-16 17:56:54 +0800

    set footer class

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,8 @@
         <div class="container">
             <a class="logo-font" href="/">Conduit</a>
             <span class="attribution">
-                An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp;
+                An interactive learning project from <a href="https://thinkster.io">Thinkster</a>.
+                Code &amp;
                 design licensed under MIT.
             </span>
         </div>

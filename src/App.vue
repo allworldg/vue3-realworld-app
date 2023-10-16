33162aaa952a08e5b83b3fa2ca5d3e16ad5e6f0a
allworldg <944937881@qq.com>
2023-10-16 17:32:29 +0800

    add footer header and types/node

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-    
+  <div>
+    <AppHeader />
+    <RouterView></RouterView>
+    <AppFooter />
+  </div>
 </template>
   
 <script setup lang='ts'>
-  
+import AppFooter from '@/components/AppFooter.vue'
+import AppHeader from '@/components/AppHeader.vue';
 </script>
   
-<style>
-  
-</style>
+<style></style>

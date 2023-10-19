ce6f0bf9eefebc84497a5838bb169f1553e45108
allworldg <944937881@qq.com>
2023-10-19 11:18:40 +0800

    add login page

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,20 +1,33 @@
 <template>
-   <div class="auth-page">
-    <div class="container page">
-        <div class="row">
-            <div class="col-md-6 offset-md-3 col-xs-12">
-                
+    <div class="auth-page">
+        <div class="container page">
+            <div class="row">
+                <div class="col-md-6 offset-md-3 col-xs-12">
+                    <h1 class="text-xs-center">Sign in</h1>
+                    <p class="text-xs-center">
+                        <RouterLink to="/register">Need an account?</RouterLink>
+                    </p>
+                    <ul class="error-messages">
+                        <li>email or password is invalid</li>
+                    </ul>
+                    <form>
+                        <fieldset class="form-group">
+                            <input class="form-control form-control-lg" type="text" placeholder="Email">
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <input class="form-control form-control-lg " type="password" placeholder="Password">
+                        </fieldset>
+                        <button class="btn btn-lg btn-primary pull-xs-right">Sign in</button>
+                    </form>
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
-   </div> 
 </template>
     
 <script setup lang='ts'>
-    
+
 </script>
     
-<style>
-    
-</style>
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
 const routes: RouteRecordRaw[] = [
-    { path: '/', component: () => { return import('@/components/Home.vue') }, children: [] }
+    { path: '/', component: () => { return import('@/components/Home.vue') }, children: [] },
+    { path: '/login', component: () => { return import('@/components/login.vue') }, children: [] }
 ]
 
 export const router = createRouter({

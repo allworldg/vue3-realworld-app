eab6117e18fa413873085bb7e7bc16fbf3e70065
allworldg <944937881@qq.com>
2023-10-20 11:46:16 +0800

    init register page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,29 @@
-import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
+import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 const routes: RouteRecordRaw[] = [
-    { path: '/', component: () => { return import('@/components/Home.vue') }, children: [] },
-    { path: '/login', component: () => { return import('@/components/login.vue') }, children: [] }
-]
+  {
+    path: "/",
+    component: () => {
+      return import("@/components/Home.vue");
+    },
+    children: [],
+  },
+  {
+    path: "/login",
+    component: () => {
+      return import("@/components/login.vue");
+    },
+    children: [],
+  },
+  {
+    path: "/register",
+    component: () => {
+      return import("@/components/register.vue");
+    },
+    children: [],
+  },
+];
 
 export const router = createRouter({
-    history: createWebHashHistory(),
-    routes: routes
-})
-
-
-
+  history: createWebHashHistory(),
+  routes: routes,
+});

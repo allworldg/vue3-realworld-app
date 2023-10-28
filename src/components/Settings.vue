fffb0322fb9dce908ce5df85b676fe1c2ac1d62b
allworldg <944937881@qq.com>
2023-10-28 20:49:21 +0800

    init static settings page

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,11 +1,55 @@
 <template>
-        this is settings
+  <div class="settings-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Your Settings</h1>
+          <ul class="error-messages">
+            <li>The name is required</li>
+          </ul>
+          <form>
+            <fieldset>
+              <fieldset class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="URL of profile picture"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  placeholder="Username"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <textarea
+                  rows="8"
+                  class="form-control form-control-lg"
+                  placeholder="short bio about you"
+                ></textarea>
+              </fieldset>
+              <fieldset class="form-group">
+                <input
+                  class="form-control form-control-lg"
+                  type="password"
+                  placeholder="New Password"
+                />
+              </fieldset>
+              <button class="btn btn-lg btn-primary pull-xs-right">
+                Update Settings
+              </button>
+            </fieldset>
+          </form>
+          <hr/>
+          <button class="btn btn-outline-danger">Or Click here to logout.</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-    
-<script setup lang='ts'>
-    
-</script>
-    
-<style>
-    
-</style>
+
+<script setup lang="ts"></script>
+
+<style></style>
